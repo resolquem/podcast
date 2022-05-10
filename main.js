97b191f3ede16ea8c6a347aee69741a804c53f19
rpadrell <coolextensions@outlook.com>
2022-05-10 13:48:40 +0200

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ let curr_track = document.createElement('audio');
 // Define the tracks that have to be played
 let track_list = [
   {
-    name: "Clash",
+    name: "SolquiCoop al Solcast",
     artist: "Me",
     image: "https://images.pexels.com/photos/2264753/pexels-photo-2264753.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
     path: "sound.mp3"
